39df647a6fd0a288e85968351b83b281bfd3c484
racibaz <r.c67@hotmail.com>
2025-06-16 16:57:08 +0300

    extenal configuration added.

---
+++ pkg/config/common.go
@@ -0,0 +1,7 @@
+package config
+
+import (
+	"github.com/racibaz/go-arch/config"
+)
+
+var configurations config.Config
---
+++ pkg/config/getter.go
@@ -0,0 +1,9 @@
+package config
+
+import (
+	"github.com/racibaz/go-arch/config"
+)
+
+func Get() config.Config {
+	return configurations
+}
