0e2cd4c4de94232f156e07e78898399cf8b2b375
racibaz <r.c67@hotmail.com>
2025-06-16 12:12:02 +0300

    google uuid package added.

---
+++ pkg/uuid/uuid.go
@@ -0,0 +1,34 @@
+package uuid
+
+import "github.com/google/uuid"
+
+type Uuid struct {
+	Uuid *uuid.UUID
+}
+
+func NewUuid() *Uuid {
+	newUuid := uuid.New()
+	return &Uuid{Uuid: &newUuid}
+}
+
+func (uuid *Uuid) ToString() string {
+	if uuid == nil || uuid.Uuid == nil {
+		return ""
+	}
+	return uuid.Uuid.String()
+}
+
+func Parse(input string) (Uuid, error) {
+	parsedUuid, err := uuid.Parse(input)
+	if err != nil {
+		return Uuid{}, err
+	}
+
+	if parsedUuid == uuid.Nil {
+		return Uuid{}, nil // or return an error if you prefer
+	}
+
+	return Uuid{
+		Uuid: &parsedUuid,
+	}, nil
+}
