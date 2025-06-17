cd4e6397e1be02447d0b35ef3270905f1d65df51
racibaz <r.c67@hotmail.com>
2025-06-17 11:16:26 +0300

    newpost factory method added to post domain

--- internal/modules/post/domain/post.go
+++ internal/modules/post/domain/post.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"errors"
 	postValueObject "github.com/racibaz/go-arch/internal/modules/post/domain/value_objects"
 	"time"
 )
@@ -12,4 +13,32 @@ type Post struct {
 	Status      postValueObject.PostStatus
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
+}
+
+func NewPost(id, title, description string, status postValueObject.PostStatus) (*Post, error) {
+
+	if id == "" {
+		return nil, errors.New("id cannot be empty")
+	}
+
+	if len(title) < 3 {
+		return nil, errors.New("title must be at least 3 characters long")
+	}
+
+	if len(description) < 10 {
+		return nil, errors.New("title must be at least 3 characters long")
+	}
+
+	if !postValueObject.IsValidPostStatus(status) {
+		return nil, errors.New("status is not valid")
+	}
+
+	return &Post{
+		ID:          id,
+		Title:       title,
+		Description: description,
+		Status:      status,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}, nil
 }
