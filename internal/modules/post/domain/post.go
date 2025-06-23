fa6bde93a663ac7981d3771319e74f4a9f96b59c
racibaz <r.c67@hotmail.com>
2025-06-23 19:29:19 +0300

    refactored the post validation

--- internal/modules/post/domain/post.go
+++ internal/modules/post/domain/post.go
@@ -8,6 +8,12 @@ import (
 )
 
 var (
+	TitleMinLength       = 10
+	DescriptionMinLength = 10
+	ContentMinLength     = 10
+)
+
+var (
 	ErrPostNotFound      = errors.New("the post was not found")
 	ErrPostAlreadyExists = errors.New("the post already exists")
 )
@@ -22,29 +28,35 @@ type Post struct {
 	UpdatedAt   time.Time
 }
 
+func (post *Post) Sanitize() {
+
+	// Trim whitespace from the input parameters
+	post.ID = strings.TrimSpace(post.ID)
+	post.Title = strings.TrimSpace(post.Title)
+	post.Description = strings.TrimSpace(post.Description)
+	post.Content = strings.TrimSpace(post.Content)
+}
+
 // Validate checks if the Post fields are valid.
 func (post *Post) Validate() error {
 
-	// Trim whitespace from the input parameters
-	id := strings.TrimSpace(post.ID)
-	title := strings.TrimSpace(post.Title)
-	description := strings.TrimSpace(post.Description)
-	content := strings.TrimSpace(post.Content)
+	// Sanitize the input parameters
+	post.Sanitize()
 
 	// Validate the input parameters
-	if id == "" {
+	if post.ID == "" {
 		return errors.New("id cannot be empty")
 	}
 
-	if len(title) < 10 {
+	if len(post.Title) < TitleMinLength {
 		return errors.New("title must be at least 3 characters long")
 	}
 
-	if len(description) < 10 {
+	if len(post.Description) < DescriptionMinLength {
 		return errors.New("description must be at least 10 characters long")
 	}
 
-	if len(content) < 10 {
+	if len(post.Content) < ContentMinLength {
 		return errors.New("content must be at least 10 characters long")
 	}
 
