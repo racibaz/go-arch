cfa743b4ee11d1841ea9372a3063f3e624f523db
racibaz <r.c67@hotmail.com>
2025-06-30 11:28:59 +0300

    Validation rules are added to post create request dto file

--- internal/modules/post/presentation/http/request_dtos/create_post_request_dto.go
+++ internal/modules/post/presentation/http/request_dtos/create_post_request_dto.go
@@ -4,9 +4,9 @@ package request_dtos
 // @Description CreatePostRequestDto is a data transfer object for creating a post
 type CreatePostRequestDto struct {
 	// @Description Title is the title of the post
-	Title string `json:"title"`
+	Title string `json:"title" validate:"required,min=10"`
 	// @Description Description is the description of the post
-	Description string `json:"description"`
+	Description string `json:"description" validate:"required,min=10"`
 	// @Description Content is the content of the post
-	Content string `json:"content"`
+	Content string `json:"content" validate:"required,min=10"`
 }
