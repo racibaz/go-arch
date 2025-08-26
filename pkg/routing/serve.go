8cd238a3ee21f038ca695797117ba7bb204adc88
racibaz <r.c67@hotmail.com>
2025-08-26 20:22:52 +0300

    added graceful shutdown for gin

--- pkg/routing/serve.go
+++ pkg/routing/serve.go
@@ -1,19 +1,47 @@
 package routing
 
 import (
+	"context"
 	"fmt"
 	"github.com/racibaz/go-arch/pkg/config"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func Serve() {
-	r := GetRouter()
 
 	configs := config.Get()
 
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", configs.Server.Port), //":8080"
+		Handler: router.Handler(),
+	}
+
 	go func() {
-		err := r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port))
-		if err != nil {
-			panic(fmt.Sprintf("Failed to start HTTP server: %v", err))
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
 		}
 	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no params) by default sends syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
