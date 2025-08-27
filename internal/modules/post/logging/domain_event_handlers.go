d8a75cf7923a6efc9f64d80ea7cc027cf9eee5ab
racibaz <r.c67@hotmail.com>
2025-08-27 20:38:08 +0300

    post event logging is updated.

--- internal/modules/post/logging/domain_event_handlers.go
+++ internal/modules/post/logging/domain_event_handlers.go
@@ -2,27 +2,28 @@ package logging
 
 import (
 	"context"
-	"github.com/racibaz/go-arch/internal/modules/post/application/handlers"
 	"github.com/racibaz/go-arch/pkg/ddd"
 	"github.com/racibaz/go-arch/pkg/logger"
 )
 
-type DomainEventHandlers struct {
-	handlers.DomainEventHandlers
+type EventHandlers[T ddd.Event] struct {
+	ddd.EventHandler[T]
+	label  string
 	logger logger.Logger
 }
 
-var _ handlers.DomainEventHandlers = (*DomainEventHandlers)(nil)
+var _ ddd.EventHandler[ddd.Event] = (*EventHandlers[ddd.Event])(nil)
 
-func LogDomainEventHandlerAccess(handlers handlers.DomainEventHandlers, logger logger.Logger) DomainEventHandlers {
-	return DomainEventHandlers{
-		DomainEventHandlers: handlers,
-		logger:              logger,
+func LogEventHandlerAccess[T ddd.Event](handlers ddd.EventHandler[T], label string, logger logger.Logger) EventHandlers[T] {
+	return EventHandlers[T]{
+		EventHandler: handlers,
+		label:        label,
+		logger:       logger,
 	}
 }
 
-func (h DomainEventHandlers) OnPostCreated(ctx context.Context, event ddd.Event) (err error) {
-	h.logger.Info("--> Post.OnPostCreated")
-	defer func() { h.logger.Error("<-- Post.OnPostCreated") }()
-	return h.DomainEventHandlers.OnPostCreated(ctx, event)
+func (h EventHandlers[T]) HandleEvent(ctx context.Context, event T) (err error) {
+	h.logger.Info("--> Post.%s.On(%s)", h.label, event.EventName())
+	defer func() { h.logger.Error("<-- Post.%s.On(%s)", h.label, event.EventName()) }()
+	return h.EventHandler.HandleEvent(ctx, event)
 }
