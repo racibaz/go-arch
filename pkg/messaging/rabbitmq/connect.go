0239614af1bea308de2021678bcf1ae8b9a2aeea
racibaz <r.c67@hotmail.com>
2025-09-25 16:10:01 +0300

    rabbitmq connection is fixed.

--- pkg/messaging/rabbitmq/connect.go
+++ pkg/messaging/rabbitmq/connect.go
@@ -7,7 +7,7 @@ import (
 
 func Connect() {
 
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
