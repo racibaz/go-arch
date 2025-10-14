c94581a0b0b81fb85b3dc50590609b26d32cfc55
racibaz <r.c67@hotmail.com>
2025-10-14 11:49:22 +0300

    fixed air issue for starting point

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Running database migrations..."
-make db_migrate_up
-
-echo "Seeding database..."
-make seed
-
 echo "Starting server with Air..."
-air
+exec air
+
+# In prod, you can use:
+#echo "Starting server"
+#./main
+
+
